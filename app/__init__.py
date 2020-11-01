ac97c63e223db7523166b36fbd479b4f59815622
rajalamgarmel <lamgarmel.raja@gmail.com>
2020-11-01 01:23:59 +0100

    Installation de flask_migrate qui va allimenter notre base de données à partir de notre model Modification du fichier __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@
 
 # third-party imports
 from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 # local imports
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 
@@ -16,13 +19,17 @@ def create_app(config_name):
     app.config.from_object(config_name)
     app.config.from_pyfile('config.py')
 
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = ""
+
+
+
     db.init_app(app)
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    migrate = Migrate(app, db)
 
-    return app
+    from app import models
+
 
     return app
