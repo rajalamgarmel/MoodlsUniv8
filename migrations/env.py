6a7063fa14e9b50ba80fce3089e430cce6d15129
rajalamgarmel <lamgarmel.raja@gmail.com>
2020-11-01 01:46:49 +0100

    Les fichiers générés par flask_migrate

--- migrations/env.py
+++ migrations/env.py
@@ -35,7 +35,7 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 
 
 def run_migrations_offline():
-    """Run migratio in 'offline' mode.
+    """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -56,7 +56,7 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """Run migratio in 'online' mode.
+    """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
